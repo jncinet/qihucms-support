36002e938eca2f21511e2d27b33828f159f0171a
张晔 <y@qilephp.com>
2020-12-28 11:18:50 +0800

    update

--- src/Str.php
+++ src/Str.php
@@ -2,16 +2,47 @@
 
 namespace Qihucms\Support;
 
+/**
+ * Class Str
+ * @package Qihucms\Support
+ */
 class Str
 {
-    public static function configEncode(string $str)
+    /**
+     * @param string $str
+     * @return false|int
+     */
+    public static function includingChinese(string $str)
     {
-
+        return preg_match("/[^\x00-\x80]/", $str);
     }
 
-//
-    public static function configDecode(string $str)
+    /**
+     * @param string $str
+     * @return bool
+     */
+    public static function isEncoding(string $str): bool
     {
-        stripos($str, 'base64:') == 0
+        return stripos($str, 'base64:') === 0;
+    }
+
+    /**
+     * @param string $str
+     * @return string
+     */
+    public static function encode(string $str): string
+    {
+        return 'base64:' . base64_encode($str);
+    }
+
+    /**
+     * @param string $str
+     * @return bool|string
+     */
+    public static function decode(string $str)
+    {
+        return self::isEncoding($str)
+            ? base64_decode(str_replace('base64:', '', $str))
+            : $str;
     }
 }
